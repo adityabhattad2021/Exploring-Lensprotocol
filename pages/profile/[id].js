0e6ab71a2b544c7f15ea402b92a8e171c560a44e
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-30 23:06:09 +0530

    Add View Publications

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
-import { client, getProfile } from "../../api";
+import { useState, useEffect, Fragment } from "react";
+import { client, getProfile, getPublications } from "../../api";
 
 function Profile() {
 	const [profile, setProfile] = useState();
+	const [publications, setPublications] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const router = useRouter();
 	const { id } = router.query;
 	useEffect(() => {
@@ -15,38 +17,75 @@ function Profile() {
 
 	async function fetchProfile() {
 		try {
-			const response = await client.query(getProfile,{id}).toPromise();
+			setIsLoading(true);
+			const response = await client.query(getProfile, { id }).toPromise();
 			console.log(response);
 			setProfile(response?.data?.profiles?.items[0]);
+
+			const publicationResponse = await client
+				.query(getPublications, { id })
+				.toPromise();
+			// console.log(publicationResponse);
+			setPublications(publicationResponse.data.publications.items);
+
+			setIsLoading(false);
 		} catch (error) {
 			console.log(error);
 		}
 	}
+	console.log(publications[0]?.metadata?.content);
 
 	return (
-		<div style={{width:"100%",display:"flex",
-		flexDirection:"column","justifyContent":"center",alignItems:"center",marginTop:"100px"}}>
-			<div>
-				{profile?.picture?.original?.url &&
-				(profile?.picture?.original?.url).includes(
-					"https://lens.infura-ipfs.io/"
-				) ? (
-					<Image
-						src={profile?.picture?.original?.url}
-						alt="Profile Picture"
-						width={200}
-						height={200}
-					/>
-				) : (
-					<div>No Image</div>
-				)}
-			</div>
-			<div style={{textAlign:"center"}}>
-				<h4>{profile?.handle}</h4>
-				<p>{profile?.bio}</p>
-				<p>Followers: {profile?.stats?.totalFollowers}</p>
-				<p>Following: {profile?.stats?.totalFollowing}</p>
-			</div>
+		<div
+			style={{
+				width: "100%",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+				marginTop: "100px",
+			}}
+		>
+			{isLoading ? (
+				<div>Loading...</div>
+			) : (
+				<Fragment>
+					<div>
+						{profile?.picture?.original?.url &&
+						(profile?.picture?.original?.url).includes(
+							"https://lens.infura-ipfs.io/"
+						) ? (
+							<Image
+								style={{ borderRadius: "100%" }}
+								src={profile?.picture?.original?.url}
+								alt="Profile Picture"
+								width={200}
+								height={200}
+							/>
+						) : (
+							<div>No Image</div>
+						)}
+					</div>
+					<div style={{ textAlign: "center" }}>
+						<h4>{profile?.handle}</h4>
+						<p>{profile?.bio}</p>
+						<p>Followers: {profile?.stats?.totalFollowers}</p>
+						<p>Following: {profile?.stats?.totalFollowing}</p>
+					</div>
+					{publications.map((publication, index) => {
+						return (
+							<div
+								style={{
+									padding: "20px",
+									borderTop: "1px solid #ededed",
+								}}
+							>
+								{publication?.metadata?.content}
+							</div>
+						);
+					})}
+				</Fragment>
+			)}
 		</div>
 	);
 }
